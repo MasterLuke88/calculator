da836286c8b23c7f8ca852c528fd04c5e0bdb3d2
luke <luke@devops.luke.lab>
2018-07-20 14:00:50 +0200

    Automatic builds

--- src/main/java/com/luke/calculator/Calculator.java
+++ src/main/java/com/luke/calculator/Calculator.java
@@ -1,6 +1,9 @@
 package com.luke.calculator;
 import org.springframework.stereotype.Service;
 
+/**
+ * Main Calculator class.
+ */
 @Service
 public class Calculator {
      int sum(int a, int b) {
